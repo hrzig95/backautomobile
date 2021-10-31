917dabd200127131fd248383b82dd16db5be3a81
hrzig95 <hrzig95@gmail.com>
2021-10-31 21:24:58 +0100

    comment voiture.getAll

--- app/controllers/voiture.controller.js
+++ app/controllers/voiture.controller.js
@@ -1,11 +1,6 @@
 const config = require("../config/config");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
 const db = require("../models");
-const User = db.user;
-const Role = db.role;
 const Voiture = db.voiture;
-const Op = db.Op;
 
 
 exports.addVoiture = (req, res) => {
@@ -22,12 +17,12 @@ exports.addVoiture = (req, res) => {
 };
 
 
-exports.allVoiture = (req, res) => {
-  Voiture.findAll()
-    .then(voitures => {
-        res.send({ voitures: voitures });
-    })
-    .catch(err => {
-      res.status(500).send({ message: err.message });
-    });
-};
+// exports.allVoiture = (req, res) => {
+//   Voiture.findAll()
+//     .then(voitures => {
+//         res.send({ voitures: voitures });
+//     })
+//     .catch(err => {
+//       res.status(500).send({ message: err.message });
+//     });
+// };
--- app/routes/voiture.routes.js
+++ app/routes/voiture.routes.js
@@ -1,5 +1,5 @@
 const { authJwt } = require("../middlewares");
-const controller = require("../controllers/user.controller");
+const controller = require("../controllers/voiture.controller");
 
 module.exports = function(app) {
   app.use(function(req, res, next) {
@@ -11,7 +11,7 @@ module.exports = function(app) {
     next();
   });
 
-  app.get("/voiture", controller.allVoiture);
+//   app.get("/voiture", controller.allVoiture);
 
 
   app.post(
