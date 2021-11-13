7d18026275ee63133042ed834f767bb259095f71
hrzig95 <hrzig95@gmail.com>
2021-11-13 16:15:52 +0100

    remove picture && equipments

--- app/controllers/voiture.controller.js
+++ app/controllers/voiture.controller.js
@@ -101,9 +101,14 @@ exports.allVoiture = (req, res) => {
 };
 
 exports.deleteVoiture = (req, res) => {
+
   Voiture.destroy( {where: {
     id: req.params.id}})
     .then(voitures => {
+      insideEquipmentVoiture.destroy({where:{voitureId:req.params.id}});
+      outsideEquipmentVoiture.destroy({where:{voitureId:req.params.id}});
+      pictureVoiture.destroy({where:{voitureId:req.params.id}});
+      securityEquipmentVoiture.destroy({where:{voitureId:req.params.id}});
         res.send({ message: "voiture was deleted successfully!" });
     })
     .catch(err => {
