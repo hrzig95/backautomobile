2d47db6bbc9163697f55791577a4c05d4348ba8b
hrzig95 <hrzig95@gmail.com>
2021-11-13 16:29:08 +0100

    change port

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ require("./app/routes/blog.routes")(app);
 
 // set port, listen for requests
 //const PORT = config.PORT;
-const PORT = 3031;
+const PORT = 3032;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
