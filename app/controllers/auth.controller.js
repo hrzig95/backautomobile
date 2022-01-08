0fa2f2a6f4476948876dd9353abeeac4f44fe863
hrzig95 <hrzig95@gmail.com>
2022-01-08 16:00:59 +0100

    add all pro users

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -10,168 +10,182 @@ const Op = db.Op;
 const { authJwt } = require("../middlewares");
 
 exports.signup = (req, res) => {
-  // Save user to database
-  let user={
-    email:req.body.email,
-    password:bcrypt.hashSync(req.body.password, 8),
-    type:req.body.type,
-    status:'active',
-    role:"user"
-  };
-  if(req.body.type==='part')
-  {
-    user.username=req.body.username;
-  }
-  if(req.body.type==='pro'){
-    user.companyName=req.body.companyName;
-    user.companyCategory=req.body.companyCategory;
-    user.companyAddress=req.body.companyAddress;
-    user.companyZipCode=req.body.companyZipCode;
-    user.companyPhone=req.body.companyPhone;
-    user.gender=req.body.gender;
-    user.firstName=req.body.firstName;
-    user.lastName=req.body.lastName;
-  }
-  User.create(user)
-    .then(user => {
-    
-          res.send({ message: "User was registered successfully!" });
-    })
-    .catch(err => {
-      res.status(500).send({ message: err.message });
-    });
+    // Save user to database
+    let user = {
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 8),
+        type: req.body.type,
+        status: 'active',
+        role: "user"
+    };
+    if (req.body.type === 'part') {
+        user.username = req.body.username;
+    }
+    if (req.body.type === 'pro') {
+        user.companyName = req.body.companyName;
+        user.companyCategory = req.body.companyCategory;
+        user.companyAddress = req.body.companyAddress;
+        user.companyZipCode = req.body.companyZipCode;
+        user.companyPhone = req.body.companyPhone;
+        user.gender = req.body.gender;
+        user.firstName = req.body.firstName;
+        user.lastName = req.body.lastName;
+    }
+    User.create(user)
+        .then(user => {
+
+            res.send({ message: "User was registered successfully!" });
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message });
+        });
 };
 
 
 exports.signin = (req, res) => {
-  User.findOne({
-    where: {
-      email: req.body.email
-    }
-  })
-    .then(user => {
-      if (!user) {
-        return res.status(404).send({ message: "User Not found." });
-      }
+    User.findOne({
+            where: {
+                email: req.body.email
+            }
+        })
+        .then(user => {
+            if (!user) {
+                return res.status(404).send({ message: "User Not found." });
+            }
 
-      let passwordIsValid = bcrypt.compareSync(
-        req.body.password,
-        user.password
-      );
+            let passwordIsValid = bcrypt.compareSync(
+                req.body.password,
+                user.password
+            );
 
-      if (!passwordIsValid) {
-        return res.status(401).send({
-          message: "Invalid Password!"
+            if (!passwordIsValid) {
+                return res.status(401).send({
+                    message: "Invalid Password!"
+                });
+            }
+            let token = jwt.sign({ id: user.id }, config.auth.secret, {
+                expiresIn: 86400 // 24 hours
+            });
+            let type = user.type ? user.type : user.role;
+            res.status(200).send({
+                id: user.id,
+                email: user.email,
+                type: type,
+                role: user.role,
+                accessToken: token,
+                expiresIn: 86400
+            });
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message });
         });
-      }
-      let token = jwt.sign({ id: user.id }, config.auth.secret, {
-        expiresIn: 86400 // 24 hours
-      });  
-      let type=user.type ? user.type : user.role;    
-        res.status(200).send({
-          id: user.id,
-          email: user.email,
-          type:type,
-          role:user.role,
-          accessToken: token,
-          expiresIn:86400
-        });
-    })
-    .catch(err => {
-      res.status(500).send({ message: err.message });
-    });
 };
 exports.allUsers = (req, res) => {
-  User.findAll({attributes: {exclude: ['password']}})
-    .then(users => {
-        res.status(200).send({users});
-    })
-    .catch(err => {
-      res.status(500).send({ message: err.message });
-    });
+    User.findAll({ attributes: { exclude: ['password'] } })
+        .then(users => {
+            res.status(200).send({ users });
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message });
+        });
 };
 
+exports.allProUsers = (req, res) => {
+    User.findAll({
+            where: {
+                type: 'pro'
+            },
+            attributes: { exclude: ['password'] }
+        })
+        .then(users => {
+            res.status(200).send({ users });
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message });
+        });
+};
 exports.getuserDetails = (req, res) => {
-  authJwt.getIdByToken(req,res);
-  let idUser=req.userId;
-  User.findOne({
-    where: {
-      id: idUser
-    },
-    attributes: {exclude: ['password']}
-  })
-    .then(user => {
-      res.status(200).send({user});
-    })
-    .catch(err => {
-      res.status(500).send({ message: err.message });
-    });
+    authJwt.getIdByToken(req, res);
+    let idUser = req.userId;
+    User.findOne({
+            where: {
+                id: idUser
+            },
+            attributes: { exclude: ['password'] }
+        })
+        .then(user => {
+            res.status(200).send({ user });
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message });
+        });
 };
 
 exports.updateInfo = (req, res) => {
-  authJwt.getIdByToken(req,res);
-  let idUser=req.userId;
-  let user={firstName:req.body.firstName,
-    lastName:req.body.lastName,
-    companyAddress:req.body.companyAddress,
-    companyPhone:req.body.companyPhone
-  }
-  User.update(user, { where: { id: idUser }})
-    .then(user => {
-      res.status(200).send({ message: "user was updated successfully!" });
-    })
-    .catch(err => {
-      res.status(500).send({ message: err.message });
-    });
+    authJwt.getIdByToken(req, res);
+    let idUser = req.userId;
+    let user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        companyAddress: req.body.companyAddress,
+        companyPhone: req.body.companyPhone
+    }
+    User.update(user, { where: { id: idUser } })
+        .then(user => {
+            res.status(200).send({ message: "user was updated successfully!" });
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message });
+        });
 };
 
 
 exports.updatePassword = (req, res) => {
-  authJwt.getIdByToken(req,res);
-  let idUser=req.userId;
-  let NewPassword=bcrypt.hashSync(req.body.newPassword, 8);
-  User.findOne({
-    where: {
-      id: idUser
-    }
-  })
-    .then(async(user) => {
-      let passwordIsValid = bcrypt.compareSync(
-        req.body.oldPassword,
-        user.password
-      );
+    authJwt.getIdByToken(req, res);
+    let idUser = req.userId;
+    let NewPassword = bcrypt.hashSync(req.body.newPassword, 8);
+    User.findOne({
+            where: {
+                id: idUser
+            }
+        })
+        .then(async(user) => {
+            let passwordIsValid = bcrypt.compareSync(
+                req.body.oldPassword,
+                user.password
+            );
 
-      if (!passwordIsValid) {
-        return res.status(401).send({
-          message: "Invalid Password!"
-        });
-      }else {
-        let user={password:NewPassword}
-        User.update(user, { where: { id: idUser }})
-          .then(newUser => {
-            res.status(200).send({ message: "password was updated successfully!" });
-          })
-          .catch(err => {
+            if (!passwordIsValid) {
+                return res.status(401).send({
+                    message: "Invalid Password!"
+                });
+            } else {
+                let user = { password: NewPassword }
+                User.update(user, { where: { id: idUser } })
+                    .then(newUser => {
+                        res.status(200).send({ message: "password was updated successfully!" });
+                    })
+                    .catch(err => {
+                        res.status(500).send({ message: err.message });
+                    });
+            }
+
+
+        })
+        .catch(err => {
             res.status(500).send({ message: err.message });
-          });
-      }
-     
-     
-    })
-    .catch(err => {
-      res.status(500).send({ message: err.message });
-    });
+        });
 };
 
 
 exports.blockUser = (req, res) => {
-  let idUser=req.params.id;
-  let user={status:"inactive"}
-  User.update(user, { where: { id: idUser }})
-    .then(newUser => {
-      res.status(200).send({ message: "user was blocked successfully!" });
-    })
-    .catch(err => {
-      res.status(500).send({ message: err.message });
-    });
+    let idUser = req.params.id;
+    let user = { status: "inactive" }
+    User.update(user, { where: { id: idUser } })
+        .then(newUser => {
+            res.status(200).send({ message: "user was blocked successfully!" });
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message });
+        });
 };
--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -2,32 +2,33 @@ const { verifySignUp } = require("../middlewares");
 const controller = require("../controllers/auth.controller");
 
 module.exports = function(app) {
-  app.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
+    app.use(function(req, res, next) {
+        res.header(
+            "Access-Control-Allow-Headers",
+            "x-access-token, Origin, Content-Type, Accept"
+        );
+
+        next();
+    });
+
+    app.post(
+        "/signup", [
+            verifySignUp.checkDuplicateUsernameOrEmail
+        ],
+        controller.signup
     );
 
-    next();
-  });
+    app.post("/signin", controller.signin);
 
-  app.post(
-    "/signup",
-    [
-      verifySignUp.checkDuplicateUsernameOrEmail],
-    controller.signup
-  );
+    app.get("/allUsers", controller.allUsers);
 
-  app.post("/signin", controller.signin);
+    app.get("/getuserDetails", controller.getuserDetails);
 
-  app.get("/allUsers", controller.allUsers);
+    app.post("/updateInfo", controller.updateInfo);
 
-  app.get("/getuserDetails", controller.getuserDetails);
+    app.post("/updatePassword", controller.updatePassword);
 
-  app.post("/updateInfo", controller.updateInfo);
-
-  app.post("/updatePassword", controller.updatePassword);
-
-  app.get("/blockUser/:id", controller.blockUser);
+    app.get("/blockUser/:id", controller.blockUser);
+    app.get("/allProUsers", controller.allProUsers);
 
 };
