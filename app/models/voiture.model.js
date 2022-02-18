341a505b329003de8a7136650a69786685978235
hrzig95 <hrzig95@gmail.com>
2022-02-18 23:03:30 +0100

    fix

--- app/models/voiture.model.js
+++ app/models/voiture.model.js
@@ -30,7 +30,7 @@ module.exports = (sequelize, Sequelize, DataTypes) => {
                 type: DataTypes.STRING,
             },
             concessionnaire: {
-                type: DataTypes.STRING,
+                type: DataTypes.INTEGER,
             },
             price: {
                 type: DataTypes.DOUBLE,
